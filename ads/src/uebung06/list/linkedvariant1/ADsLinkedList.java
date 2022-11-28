f6c4025053001579db6324474cf0933f6e1b70cc
vollkornholztuer <mcretella2012@gmail.com>
2022-11-28 12:57:38 +0100

    ue06 - Verzweifelter versuch :(

--- ads/src/uebung06/list/linkedvariant1/ADsLinkedList.java
+++ ads/src/uebung06/list/linkedvariant1/ADsLinkedList.java
@@ -26,35 +26,65 @@ public class ADsLinkedList implements ADsList {
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+        int count = 0;
+        ADsListNode node = head.next;
+        while (node != null) {
+            count++;
+            node = node.next;
+        }
+        return count;
     }
 
     @Override
     public void insert(int pos, Object elem) {
-        // TODO Auto-generated method stub
+
+        if (pos == 0) {
+            if (head == null) {
+                head = new ADsListNode(elem);
+            } else {
+                ADsListNode node = new ADsListNode(elem);
+                node.next = head;
+                head = node;
+            }
+        } else if (pos > 0) {
+            ADsListNode iterator = head;
+            int count = 0;
+            while ((count != pos - 1) && (iterator != null)) {
+                count++;
+                iterator = iterator.next;
+            }
+            if (iterator != null) {
+                ADsListNode node = new ADsListNode(elem);
+                node.next = iterator.next;
+                iterator.next = node;
+            } else {
+                ; // Error: pos > number of elements: invalid position
+            }
+        } else {
+            ; // Error: pos < 0: invalid position
+        }
+
     }
 
     @Override
     public void remove(int pos) {
-        // TODO Auto-generated method stub
+
     }
 
     @Override
     public Object elementAt(int pos) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public int find(Object element) {
-        // TODO Auto-generated method stub
+
         return 0;
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
+
         return false;
     }
 }
